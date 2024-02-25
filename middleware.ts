943894fa4575dff8593523a7c01933eec44ea1f3
MarcoFiesque <m.fiesque@taman.fr>
2024-02-25 13:18:57 +0100

    ...

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: ['/', "/api/webhooks(.*)"],
-    ignoredRoutes: ['/tests', '/api/webhooks/clerk'],
+    // ignoredRoutes: ['/tests', '/api/webhooks/clerk'],
 
     // ignoredRoutes: ['/no-auth-in-this-route'],
     // 
